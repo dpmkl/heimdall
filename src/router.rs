f6b383d85d8151b0d2ef6040b6cd55d5d5c106fb
Michael Craggs <m.e.craggs@gmail.com>
2019-12-19 21:31:22 +0100

    Fix route evaluation to use hyper::Uri instead of manual assembly

--- src/router.rs
+++ src/router.rs
@@ -1,8 +1,10 @@
 use crate::acl::{parse_allowed_methods, AllowedMethods};
 use crate::config::Config;
-use hyper::{Body, Request};
+use hyper::http::uri::{Authority, Scheme};
+use hyper::{Body, Request, Uri};
 use path_tree::PathTree;
 use std::net::SocketAddr;
+use std::str::FromStr;
 
 fn make_path(path: String) -> String {
     let mut path = path.replace("//", "/");
@@ -29,7 +31,7 @@ pub struct Router {
 
 #[derive(Debug, PartialEq)]
 pub enum RouterResult {
-    Success(String),
+    Success(Uri),
     NotDefined,
     NotAllowedMethod,
 }
@@ -52,23 +54,44 @@ impl Router {
 
     pub fn eval(&self, req: &Request<Body>) -> RouterResult {
         if let Some(node) = self.routes.find(req.uri().path()) {
-            if node.0.allowed_methods == AllowedMethods::Any
-                || node.0.allowed_methods.contains(req.method())
+            let target = node.0;
+            if target.allowed_methods == AllowedMethods::Any
+                || target.allowed_methods.contains(&req.method())
             {
-                let target = node.0;
-                let route = if let Some(path) = &node.0.path {
-                    format!("http://{}:{}{}", node.0.addr.ip(), target.addr.port(), path)
+                let uri = Uri::builder().scheme(Scheme::HTTP);
+                let uri = uri.authority(
+                    Authority::from_str(&format!("{}:{}", target.addr.ip(), target.addr.port()))
+                        .unwrap(),
+                );
+                let params = node
+                    .1
+                    .iter()
+                    .map(|p| format!("/{}", p.1))
+                    .collect::<String>();
+                println!("params: {:?}", params);
+                let p_and_q = if let Some(p_and_q) = req.uri().path_and_query() {
+                    let path = match &target.path {
+                        Some(path) => path,
+                        None => "",
+                    };
+                    if let Some(query) = p_and_q.query() {
+                        format!("{}{}?{}", path, params, query)
+                    } else {
+                        format!("{}{}", path, params)
+                    }
                 } else {
-                    format!("http://{}:{}", node.0.addr.ip(), target.addr.port())
+                    println!("No path and query");
+                    String::default()
                 };
-                if node.1.is_empty() {
-                    RouterResult::Success(route)
-                } else {
-                    let ext: Vec<String> =
-                        node.1.into_iter().map(|(_, v)| format!("/{}", v)).collect();
-                    println!("{:?}", ext);
-                    RouterResult::Success(format!("{}{}", route, ext.join("/")))
-                }
+                println!(
+                    "{:?} -> {} | {:?}",
+                    req.uri().path_and_query(),
+                    p_and_q,
+                    node.1
+                );
+                let p_and_q: &str = &p_and_q;
+                let uri = uri.path_and_query(p_and_q).build().unwrap();
+                RouterResult::Success(uri)
             } else {
                 RouterResult::NotAllowedMethod
             }
@@ -85,12 +108,18 @@ impl Router {
     }
 
     #[cfg(test)]
-    pub fn add_route(&mut self, source: &str, addr: SocketAddr, allowed_methods: AllowedMethods) {
+    pub fn add_route(
+        &mut self,
+        source: &str,
+        addr: SocketAddr,
+        allowed_methods: AllowedMethods,
+        path: Option<String>,
+    ) {
         self.routes.insert(
             &make_path(source.to_owned()),
             Target {
                 addr,
-                path: None,
+                path,
                 allowed_methods,
             },
         );
@@ -99,9 +128,8 @@ impl Router {
 
 #[cfg(test)]
 mod tests {
-    use super::AllowedMethods;
-    use super::{Router, RouterResult};
-    use hyper::{Body, Method, Request};
+    use super::{AllowedMethods, Router, RouterResult};
+    use hyper::{Body, Method, Request, Uri};
 
     fn build_req(uri: &str, method: hyper::Method) -> Request<Body> {
         Request::builder()
@@ -115,36 +143,52 @@ mod tests {
     fn route_evaluation() {
         let root = "0.0.0.0:8080".parse().unwrap();
         let home = "0.0.0.0:8000".parse().unwrap();
-        let site = "127.0.0.1:5000".parse().unwrap();
-        let bulk = "127.0.0.1:3000".parse().unwrap();
-        let multi = "127.0.0.1:2000".parse().unwrap();
+        let path = "0.0.0.0:7000".parse().unwrap();
+        let site = "0.0.0.0:5000".parse().unwrap();
+        let bulk = "0.0.0.0:3000".parse().unwrap();
         let mut router = Router::new();
-        router.add_route("/", root, AllowedMethods::Any);
-        router.add_route("/home", home, AllowedMethods::Only(vec![Method::GET]));
-        router.add_route("/home/*any", home, AllowedMethods::Any);
-        router.add_route("/site/:name", site, AllowedMethods::Any);
-        router.add_route("/bulk/*any", bulk, AllowedMethods::Any);
-        router.add_route("/multi/:name/res/:res", multi, AllowedMethods::Any);
-        
+        router.add_route("/", root, AllowedMethods::Any, None);
+        router.add_route("/home", home, AllowedMethods::Only(vec![Method::GET]), None);
+        router.add_route(
+            "/specific",
+            path,
+            AllowedMethods::Any,
+            Some("/foobar".to_owned()),
+        );
+        router.add_route("/bulk/*any", bulk, AllowedMethods::Any, None);
+        router.add_route("/site/:name", site, AllowedMethods::Any, None);
+
         assert_eq!(
             router.eval(&build_req("/", Method::GET)),
-            RouterResult::Success("http://0.0.0.0:8080".to_owned())
+            RouterResult::Success(Uri::from_static("http://0.0.0.0:8080"))
         );
         assert_eq!(
             router.eval(&build_req("/home", Method::GET)),
-            RouterResult::Success("http://0.0.0.0:8000".to_owned())
+            RouterResult::Success(Uri::from_static("http://0.0.0.0:8000"))
         );
         assert_eq!(
-            router.eval(&build_req("/home/qwerty", Method::GET)),
-            RouterResult::Success("http://0.0.0.0:8000/qwerty".to_owned())
+            router.eval(&build_req("/home?asdf=foobar", Method::GET)),
+            RouterResult::Success(Uri::from_static("http://0.0.0.0:8000/?asdf=foobar"))
         );
         assert_eq!(
-            router.eval(&build_req("/home/asdf/qwerty", Method::GET)),
-            RouterResult::Success("http://0.0.0.0:8000/asdf/qwerty".to_owned())
+            router.eval(&build_req("/home/asdf", Method::GET)),
+            RouterResult::NotDefined
+        );
+        assert_eq!(
+            router.eval(&build_req("/bulk/qwerty", Method::GET)),
+            RouterResult::Success(Uri::from_static("http://0.0.0.0:3000/qwerty"))
+        );
+        assert_eq!(
+            router.eval(&build_req("/bulk/asdf/qwerty", Method::GET)),
+            RouterResult::Success(Uri::from_static("http://0.0.0.0:3000/asdf/qwerty"))
         );
         assert_eq!(
             router.eval(&build_req("/home", Method::POST)),
             RouterResult::NotAllowedMethod
+        );
+        assert_eq!(
+            router.eval(&build_req("/specific", Method::GET)),
+            RouterResult::Success(Uri::from_static("http://0.0.0.0:7000/foobar"))
         );
         assert_eq!(
             router.eval(&build_req("/notdefined", Method::GET)),
